cdc1e2d561f56224d1f0b8de60a012e018af8b09
Ibrahim Arafeh <ibrahimarafeh5@gmail.com>
2022-10-10 18:29:15 +0400

    Get Ram Value for Machine and SQLServer has done

--- NSCS/NSCS/Program.cs
+++ NSCS/NSCS/Program.cs
@@ -1,10 +1,19 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Management.Infrastructure;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Management;
 using System.Text.RegularExpressions;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
+    public static List<string> stRam = new List<string>();
+
     private static void Main(string[] args)
     {
         string[] ConfigLines = System.IO.File.ReadAllLines(@"config.txt");
@@ -16,11 +25,15 @@ internal class Program
         string trxSrvBosFilePath = ConfigLines[5].ToString();
         string posMachinePath = ConfigLines[6].ToString();
         string trxSrvPosFilePath = ConfigLines[7].ToString();
+        int BosRamCompareValue = Convert.ToInt32(ConfigLines[8]);
+        int SqlRamCompareValue = Convert.ToInt32(ConfigLines[9]);
 
         string[] Stationslines = System.IO.File.ReadAllLines(@"stationsIDs.txt");
         string stationEODPath = "";
         string stationTrxSrvBOSPath = "";
         string stationTrxSrvPOSPath = "";
+     
+
         foreach(string Stationline in Stationslines)
         {
             string sationID = string.Join("", Stationline.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
@@ -31,9 +44,14 @@ internal class Program
             {
                 try
                 {
-                    // this for test
+
+                    // //this for test
                     //var t0 = Task.Run(() => CheckOnLogFile(@"C:\\SY_PROCS.TXT", firstLine, sationID, serviceType, sationID + "" + machinePath));
                     //t0.Wait();
+
+                    //var tGetRam = Task.Run(() => getRamUsage(".", sationID, RamCompareValue));
+                    //tGetRam.Wait();
+
 
                     //using (StreamWriter w = File.AppendText("OptTrxSrv_Success_Log.txt"))
                     //{
@@ -51,24 +69,32 @@ internal class Program
                     //}
 
 
-                    var t = Task.Run(() => CheckOnLogFile(stationTrxSrvBOSPath, firstLine, sationID, serviceType, sationID + "" + machinePath));
-                    t.Wait();
-
-                    using (StreamWriter w = File.AppendText("OptTrxSrv_Success_Log.txt"))
-                    {
-                        Console.WriteLine(sationID + " --> BOS ----> Done");
-                        wirteLog(sationID, "--> BOS ---> Done", w); ;
-                    }
-
-                    var t2 = Task.Run(() => CheckOnLogFile(stationTrxSrvPOSPath, firstLine, sationID, serviceType, sationID + "" + posMachinePath));
-                    t2.Wait();
 
 
-                    using (StreamWriter w = File.AppendText("OptTrxSrv_Success_Log.txt"))
-                    {
-                        Console.WriteLine(sationID + " ---> POS ----> Done");
-                        wirteLog(sationID, "---> POS -- Done", w); ;
-                    }
+
+
+                    //var t = Task.Run(() => CheckOnLogFile(stationTrxSrvBOSPath, firstLine, sationID, serviceType, sationID + "" + machinePath));
+                    //t.Wait();
+
+                    //var tGetRam = Task.Run(() => getRamUsage(machinePath, sationID, RamCompareValue));
+                    //tGetRam.Wait();
+
+
+                    //using (StreamWriter w = File.AppendText("OptTrxSrv_Success_Log.txt"))
+                    //{
+                    //    Console.WriteLine(sationID + " --> BOS ----> Done");
+                    //    wirteLog(sationID, "--> BOS ---> Done", w); ;
+                    //}
+
+                    //var t2 = Task.Run(() => CheckOnLogFile(stationTrxSrvPOSPath, firstLine, sationID, serviceType, sationID + "" + posMachinePath));
+                    //t2.Wait();
+
+
+                    //using (StreamWriter w = File.AppendText("OptTrxSrv_Success_Log.txt"))
+                    //{
+                    //    Console.WriteLine(sationID + " ---> POS ----> Done");
+                    //    wirteLog(sationID, "---> POS -- Done", w); ;
+                    //}
 
                 }
                 catch (Exception ex)
@@ -89,8 +115,24 @@ internal class Program
                     //var t = Task.Run(() => CheckOnLogFile(@"C:\\DayEnd.1.log", firstLine, Stationline));
                     //t.Wait();
 
-                    var t = Task.Run(() => CheckOnLogFile(stationEODPath, firstLine, sationID,serviceType,""));
-                    t.Wait();
+                    var tGet2323Ram = Task.Run(() => getRamUsage(".", ""));
+                    tGet2323Ram.Wait();
+
+                    var tSqlRam = Task.Run(() => getSqlRam("SP02B01ZZ4864A", sationID, SqlRamCompareValue));
+                    tSqlRam.Wait();
+
+
+
+
+                    //var t = Task.Run(() => CheckOnLogFile(stationEODPath, firstLine, sationID, serviceType, ""));
+                    //t.Wait();
+
+                    //var tGetRam = Task.Run(() => getRamUsage(machinePath, sationID));
+                    //tGetRam.Wait();
+
+
+                    //var tSqlRam = Task.Run(() => getSqlRam(sationID + machinePath, sationID, SqlRamCompareValue));
+                    //tGetRam.Wait();
 
 
                     using (StreamWriter w = File.AppendText("SuccessLog.txt"))
@@ -106,7 +148,6 @@ internal class Program
                     {
                         wirteLog(sationID, ex.Message, w);
                     }
-
                 }
             }
             
@@ -162,7 +203,6 @@ internal class Program
                                 wirteLog(detectedDate, stationID, w);
                             }
                         }
-
                     }
                 }
                 
@@ -176,13 +216,108 @@ internal class Program
                 wirteLog(stationID , ex.Message, w);
             }
         }
-      
+
+        //getRamUsage(machineName,stationID);
+
     }
 
     public static void wirteLog(string LogDate, string stationNO, TextWriter w)
     {
         w.WriteLine(stationNO + "|" + LogDate);
     }
+
+    public async static void getRamUsage(string comName,string stationID)
+    {
+        double RamValue = 0;
+        try
+        {
+            ConnectionOptions connection = new ConnectionOptions();
+            connection.Impersonation = ImpersonationLevel.Impersonate;
+            string machinePath = @"\\" + stationID + comName + "\\root\\CIMV2";
+            ManagementScope scope = new ManagementScope(machinePath, connection);
+            scope.Connect();
+
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_ComputerSystem");
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                RamValue = Convert.ToUInt64(queryObj["TotalPhysicalMemory"]) / (1024 * 1024);
+                Console.WriteLine(stationID + " ---> " + "Ram size :" + RamValue);
+                    using (StreamWriter w = File.AppendText("RAM_Result.txt"))
+                    {
+                        wirteLog(RamValue.ToString(), stationID,w);
+                    }
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message.ToString());
+            using (StreamWriter w = File.AppendText("Ram_ErrorLog.txt"))
+            {
+                wirteLog(stationID, ex.Message, w);
+            }
+        }
+       
+    }
+
+
+    public async static void getSqlRam(string comName, string stationID, int RamCompareValue)
+    {
+        double RamValue = 0;
+        string sqlConn = @"Data Source=" + comName + ";Initial Catalog=Master;Integrated Security=True";
+     
+        try
+        {
+            using (var connection = new SqlConnection(sqlConn))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand cmd;
+                    SqlDataReader dataReader;
+                    string sql = "";
+
+                    sql = "select physical_memory_kb/1024 from sys.dm_os_sys_info;";
+                    cmd = new SqlCommand(sql, connection);
+                    dataReader = cmd.ExecuteReader();
+                    while(dataReader.Read())
+                    {
+                        var output= (int)(long)dataReader.GetInt64(0);
+                        using (StreamWriter w = File.AppendText("Sql_CmdRam.txt"))
+                        {
+                            Console.WriteLine(stationID + " --> " + " SqlRam" + ": "+ output);
+                            wirteLog(output.ToString(),stationID, w);
+                        }
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    using (StreamWriter w = File.AppendText("log.txt"))
+                    {
+                        Console.WriteLine(ex.Message);
+                        wirteLog(stationID, ex.Message,w);
+                        throw;
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message.ToString());
+            using (StreamWriter w = File.AppendText("Ram_ErrorLog.txt"))
+            {
+                wirteLog(stationID, ex.Message, w);
+            }
+        }
+
+    }
+
+   
+
 
 
 }
